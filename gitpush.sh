223401aeadc2d194328dfeeb0e315c144acb0d3b
nozatech <albertpark5@gmail.com>
2018-08-03 11:44:16 -0700

    update

--- gitpush.sh
+++ gitpush.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
+
+echo "Checking if there is any changes...."
 git status
+
 sleep 3
+
+
+echo "Newly changed files are adding to the stage...."
 git add .
+
+echo "Committing to Stage...."
 git commit -m "update"
+
+echo "Uploading to Github...."
 git push
 
 echo "All files are uploaded to Gibhub." 
-     "Check your new files on the Gibhub."
+echo "Check your new files on the Gibhub."
